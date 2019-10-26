4834fb77022f5cd0dd76ad1166630011da368141
PeepsCallMeLushi <41750748+PeepsCallMeLushi@users.noreply.github.com>
2019-10-26 23:13:47 +0100

    added material and shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { AngularMaterialModule } from './shared/angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
+    AngularMaterialModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
---
+++ src/app/shared/angular-material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatTabsModule,
+  MatToolbarModule,
+  MatCardModule,
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatCardModule
+  ]
+})
+export class AngularMaterialModule { }
