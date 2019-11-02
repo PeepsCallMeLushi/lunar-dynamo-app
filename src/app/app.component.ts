4fd617c9a54473cbfd7de36e73acce43fa31ea16
PeepsCallMeLushi <41750748+PeepsCallMeLushi@users.noreply.github.com>
2019-11-02 02:40:27 +0000

    added UI references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lunar-dynamo-app';
   public width: number = window.innerWidth;
+  public isOpen: boolean = this.width >= 960;
 
   constructor () {
+
     window.addEventListener('resize', ()=>{
       this.width = window.innerWidth;
     });
--- src/app/elements/side-nav/side-nav.component.ts
+++ src/app/elements/side-nav/side-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SideNavElement } from 'src/app/shared/models/side-nav-element.model';
 import { MatSidenav } from '@angular/material';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
@@ -10,26 +11,40 @@ import { MatSidenav } from '@angular/material';
 export class SideNavComponent implements OnInit {
 
   @Input() public inputSideNav: MatSidenav;
+  public url: string = '/';
 
   public sideNavElements: SideNavElement[] = [
     {
+      icon: 'home',
+      text: 'Home',
+      route: '/',
+    },
+    {
       icon: 'input',
       text:'iLevel Calculator',
-      route: 'iLevel-Calculator',
+      route: '/iLevel-Calculator',
     },
     {
       icon: 'link',
       text: 'Links',
-      route: 'links',
+      route: '/links',
     },
     {
       icon: 'description',
       text: 'Rules',
-      route: 'fc-info'
+      route: '/fc-info'
     }
   ]
 
-  constructor() { }
+  constructor(
+    private route: Router
+  ) {
+    this.route.events.subscribe((data)=>{
+      if (data instanceof NavigationStart) {
+        this.url = data.url;
+      }
+    });
+  }
 
   ngOnInit() {
   }
